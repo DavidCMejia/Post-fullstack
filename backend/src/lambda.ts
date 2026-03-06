ad106b242c119b7997e609b0b8449df6b55d8e42
David <dcmejiaz@geosat.com.co>
2026-03-06 08:15:14 -0500

    fix en lambda y readme

--- backend/src/lambda.ts
+++ backend/src/lambda.ts
@@ -1,4 +1,12 @@
 import serverless from 'serverless-http'
 import app from './app'
+import { AppDataSource } from './config/dataSource'
 
-export const handler = serverless(app)
+const serverlessApp = serverless(app)
+
+export const handler = async (event: any, context: any) => {
+  if (!AppDataSource.isInitialized) {
+    await AppDataSource.initialize()
+  }
+  return serverlessApp(event, context)
+}
